31d52de3266fcdc2d28140cf06b21dc6bd6cc23a
quliangliang <249180439@qq.com>
2019-11-28 20:07:29 +0800

    验证括号合法，两个集合合并

--- src/main/java/com/myself/letcode/IsValid.java
+++ src/main/java/com/myself/letcode/IsValid.java
@@ -1,10 +1,48 @@
 package com.myself.letcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Stack;
+
 /**
  * @program: jubian-cloud->IsValid
  * @description: 有效的括号
+ * 使用数据结构栈
  * @author: qll
  * @create: 2019-11-28 14:51
  **/
 public class IsValid {
+
+    static Map<Character,Character> map = new HashMap<>();
+    static {
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+    }
+    public static void main(String[] args) {
+        System.out.println(isValid("["));
+    }
+    public static boolean isValid(String s) {
+        //初始化栈
+        Stack<Character> stack = new Stack<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if(Objects.nonNull(map.get(chars[i]))){
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character pop = stack.pop();
+                if(pop == map.get(chars[i])){
+                    continue;
+                }
+                else {
+                    return false;
+                }
+            }else {
+                stack.push(chars[i]);
+            }
+        }
+        return stack.isEmpty();
+    }
 }
---
+++ src/main/java/com/myself/letcode/MergeTwoLists.java
@@ -0,0 +1,65 @@
+package com.myself.letcode;
+
+/**
+ * @program: jubian-cloud->MergeTwoLists
+ * @description: 连个集合合并
+ * @author: qll
+ * @create: 2019-11-28 18:53
+ **/
+public class MergeTwoLists {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(6);
+        ListNode l2 = new ListNode(3);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(5);
+        ListNode listNode = mergeTwoLists2(l1, l2);
+        System.out.println(listNode);
+    }
+
+    private static ListNode mergeTwoLists(ListNode l1,ListNode l2){
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+        if(l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+
+    private static ListNode mergeTwoLists2(ListNode l1,ListNode l2){
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
