61b489029c0ca9c8500ef755fbf4f022ed4e8dbc
quliangliang <249180439@qq.com>
2019-11-28 20:47:35 +0800

    数据去重

---
+++ src/main/java/com/myself/letcode/RemoveDuplicates.java
@@ -0,0 +1,25 @@
+package com.myself.letcode;
+
+/**
+ * @program: jubian-cloud->RemoveDuplicates
+ * @description: 删除数组中重复值
+ * 方法：双指针法
+ * @author: qll
+ * @create: 2019-11-28 20:08
+ **/
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+    }
+    private static int removeDuplicates(int[] nums){
+        if(nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if(nums[i] != nums[j]){
+                nums[i + 1] = nums[j];
+                i ++;
+            }
+        }
+        return i;
+    }
+}
